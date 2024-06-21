309774701282fced07268217fd03fa287f537467
gustavo.p07 <gustavo.p07@aluno.ifsc.edu.br>
2024-06-21 20:20:42 -0300

    :sparkles: feat: verificação de cpf valido

--- vendedor.py
+++ vendedor.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 import re
+import validate_cpf
 
 db = SQLAlchemy()
 
@@ -19,7 +20,7 @@ class Vendedor(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    cpf = db.Column(db.String(14), unique=True, nullable=False)
     data_nascimento = db.Column(db.String(10), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     estado = db.Column(db.String(2), nullable=False)
@@ -44,7 +45,7 @@ class Vendedor(db.Model):
             raise ValueError("Email inválido")
 
         # Validação de CPF (apenas números e 11 dígitos)
-        if not re.match(r"^\d{11}$", cpf):
+        if not validate_cpf.is_valid(cpf):
             raise ValueError("CPF inválido")
 
         vendedor = cls(nome=nome, cpf=cpf, data_nascimento=data_nascimento, email=email, estado=estado)
